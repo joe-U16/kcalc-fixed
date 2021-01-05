54867b3b2148e10307ca037c3fa5f07570095770
joe-U16 <tand23744@gmail.com>
2021-01-05 16:14:20 +0800

    Implement the function that can check the answer

--- eval.c
+++ eval.c
@@ -9,16 +9,58 @@ int main(int argc, char *argv[])
     if (argc < 2)
         return -1;
 
+    // if (argv[2] == NULL) {
+    //     argv[2] = "no";
+    // }
+
     fixedp ipt = {0};
     ipt.data = strtoul(argv[1], NULL, 10);
+    char k_result[8];
+    double result = 1;
 
     if (ipt.data == NAN_INT)
-        puts("NAN_INT");
+        strncpy(k_result, "NAN_INT", 7);
     else if (ipt.data == INF_INT)
-        puts("INF_INT");
+        strncpy(k_result, "INF_INT", 7);
     else {
-        double result = (int) ipt.inte + ((double) ipt.frac / UINT32_MAX);
-        printf("%lf\n", result);
+        result = (int) ipt.inte + ((double) ipt.frac / UINT32_MAX);
     }
+
+    if (argc == 2) {
+        if (result) {
+            printf("%lf\n", result);
+        } else {
+            printf("%s\n", k_result);
+        }
+        return 0;
+    }
+
+    size_t ans_len = strlen(argv[2]);
+
+    int bool_nan = strcmp("NAN_INT", argv[2]);
+    int bool_inf = strcmp("INF_INT", argv[2]);
+
+    if (bool_nan == 0) {
+        if (strncpy(k_result, argv[2], ans_len))
+            printf("FAIL: %s != NAN_INT\n", k_result);
+        else
+            printf("OK: NAN_INT == NAN_INT\n");
+    } else if (bool_inf == 0) {
+        if (strncpy(k_result, argv[2], ans_len))
+            printf("FAIL: %s != INF_INT\n", k_result);
+        else
+            printf("OK: NAN_INT == INF_INT\n");
+
+    } else {
+        double test = strtod(argv[2], NULL);
+        printf("result = %lf\n", result);
+        printf("result = %lf\n", result);
+
+        if (result != test)
+            printf("FAIL: %lf != %lf\n", result, test);
+        else
+            printf("OK: %lf == %lf\n", result, test);
+    }
+
     return 0;
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -6,10 +6,12 @@ CALC_MOD=calc.ko
 EVAL=./eval
 
 test_op() {
-    local expression=$1 
-    echo "Testing " ${expression} "..."
+    echo $'\0'
+
+    local expression=$1
+    echo "Testing " ${expression} "..." 
     echo -ne ${expression}'\0' > $CALC_DEV
-    $EVAL $(cat $CALC_DEV)
+    $EVAL $(cat $CALC_DEV) $2
 }
 
 if [ "$EUID" -eq 0 ]
@@ -26,45 +28,45 @@ sudo chmod 0666 $CALC_DEV
 echo
 
 # multiply
-test_op '6*7'
+test_op '6*7' '42'
 
 # add
-test_op '1980+1'
+test_op '1980+1' '1981'
 
 # sub
-test_op '2019-1'
+test_op '2019-1' '2018'
 
 # div
-test_op '42/6'
-test_op '1/3'
-test_op '1/3*6+2/4'
-test_op '(1/3)+(2/3)'
-test_op '(2145%31)+23'
-test_op '0/0' # should be NAN_INT
+test_op '42/6' '7'
+test_op '1/3' '0.333333'
+test_op '1/3*6+2/4' '2.5'
+test_op '(1/3)+(2/3)' '1'
+test_op '(2145%31)+23' '29'
+test_op '0/0' 'NAN_INT'# should be NAN_INT
 
 # binary
-test_op '(3%0)|0' # should be 0
-test_op '1+2<<3' # should be (1 + 2) << 3 = 24
-test_op '123&42' # should be 42
-test_op '123^42' # should be 81
+test_op '(3%0)|0' '0' # should be 0
+test_op '1+2<<3' '24' # should be (1 + 2) << 3 = 24
+test_op '123&42' '42' # should be 42
+test_op '123^42' '81' # should be 81
 
 # parens
-test_op '(((3)))*(1+(2))' # should be 9
+test_op '(((3)))*(1+(2))' '9' # should be 9
 
 # assign
-test_op 'x=5, x=(x!=0)' # should be 1
-test_op 'x=5, x = x+1' # should be 6
+test_op 'x=5, x=(x!=0)' '1' # should be 1
+test_op 'x=5, x = x+1' '1' # should be 6
 
 # fancy variable name
-test_op 'six=6, seven=7, six*seven' # should be 42
-test_op '小熊=6, 維尼=7, 小熊*維尼' # should be 42
-test_op 'τ=1.618, 3*τ' # should be 3 * 1.618 = 4.854
-test_op '$(τ, 1.618), 3*τ()' # shold be 3 * 1.618 = 4.854
+test_op 'six=6, seven=7, six*seven' '42' # should be 42
+test_op '小熊=6, 維尼=7, 小熊*維尼' '42' # should be 42
+test_op 'τ=1.618, 3*τ' '4.854000' # should be 3 * 1.618 = 4.854
+test_op '$(τ, 1.618), 3*τ()' '4.854' # shold be 3 * 1.618 = 4.854
 
 # functions
-test_op '$(zero), zero()' # should be 0
-test_op '$(one, 1), one()+one(1)+one(1, 2, 4)' # should be 3
-test_op '$(number, 1), $(number, 2+3), number()' # should be 5
+test_op '$(zero), zero()' '0' # should be 0
+test_op '$(one, 1), one()+one(1)+one(1, 2, 4)' '3' # should be 3
+test_op '$(number, 1), $(number, 2+3), number()' '5' # should be 5
 
 # pre-defined function
 test_op 'nop()'
